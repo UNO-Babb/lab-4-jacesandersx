ccd804ed8d0f5e9ae668facd806937eabcd1adfe
jacesandersx <jacesanders@unomaha.edu>
2024-09-18 12:17:28 -0500

    Update TurtleGraphics.py

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -34,43 +34,44 @@ drawOctagon(bob)
     
 
     # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
-hideturtle()
 bob = turtle.Turtle()
+hideturtle()  
 col = input("Enter color: ")
-def drawSquare(myTurtle):
+corner = input("Which Corner : ")
+def drawSquare(myTurtle, size):
     for i in range(4):
-        myTurtle.forward(100)
+        myTurtle.forward(size)
         myTurtle.right(90)
-drawSquare(bob)
-bob.penup()
-bob.forward(50)
-bob.pendown()
-bob.begin_fill()
-bob.fillcolor(col)
-bob.right(90)
-def drawSquare(myTurtle):
-    for i in range(4):
-        myTurtle.forward(50)
-        myTurtle.left(90)
-drawSquare(bob)
-bob.end_fill()
-    # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
-hideturtle()
-bob = turtle.Turtle()
-col = input("Enter color: ")
-def drawSquare(myTurtle):
-    for i in range(4):
-        myTurtle.forward(100)
-        myTurtle.right(90)
-drawSquare(bob)
-bob.fillcolor(col)
-bob.begin_fill()
-def drawSquare(myTurtle):
-    for i in range(4):
-        myTurtle.forward(50)
-        myTurtle.right(90)
-drawSquare(bob)
-bob.end_fill()
+drawSquare(bob, 100)
+if corner == "1":
+    bob.fillcolor(col)
+    bob.begin_fill()
+    drawSquare(bob, 50)  
+    bob.end_fill()  
+if corner == "2":
+    bob.penup()
+    bob.goto(50,0)
+    bob.pendown()
+    bob.fillcolor(col)
+    bob.begin_fill()
+    drawSquare(bob, 50)  
+    bob.end_fill()  
+if corner == "3":
+    bob.penup()
+    bob.goto(0,-50)
+    bob.pendown()
+    bob.fillcolor(col)
+    bob.begin_fill()
+    drawSquare(bob, 50)  
+    bob.end_fill()  
+if corner == "4":
+    bob.penup()
+    bob.goto(50,-50)
+    bob.pendown()
+    bob.fillcolor(col)
+    bob.begin_fill()
+    drawSquare(bob, 50)  
+    bob.end_fill()  
 
     # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
 hideturtle()
